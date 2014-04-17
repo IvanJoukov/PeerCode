e123539cb7b6d00cadc565369105d115dd7b5362
IvanJoukov <ivan.joukov@gmail.com>
2014-04-17 01:40:11 -0400

    Fixed some styling issues and got audio/video chat working

--- Scripts/PeerCode.js
+++ Scripts/PeerCode.js
@@ -1,5 +1,6 @@
 ﻿var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
+var video = document.getElementById("live_video");
 
 function setEditorLanguage (language)
 {
@@ -45,12 +46,36 @@ $(function () {
     $('#connectBtn').click(event, function () {
         var conn = peer.connect(document.getElementById("targetPeerID").value);
         communicate(conn);
+
+        // Call a peer, providing our mediaStream
+        navigator.webkitGetUserMedia({ video: true, audio: true },
+            function (mediaStream) {
+                var call = peer.call(document.getElementById("targetPeerID").value, mediaStream);
+                call.on('stream', function (stream) {
+                    video.src = window.webkitURL.createObjectURL(stream);
+                });
+
+            }
+        );
     });
 
 
     // Incoming
     peer.on('connection', function (conn) {
         communicate(conn);
+    });
+
+    peer.on('call', function (call) {
+        debugger;
+        // Answer the call, providing our mediaStream
+        navigator.webkitGetUserMedia({ video: true, audio: true },
+             function (mediaStream) {
+                 call.answer(mediaStream);
+                 call.on('stream', function (stream) {
+                     video.src = window.webkitURL.createObjectURL(stream);
+                 });
+             }
+         );
     });
 
     var languageDropdown = document.getElementById("mode");
